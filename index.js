b3539452dd275c48ade72f423598e70aa445c65d
miriam-sullivan <maljayd42@gmail.com>
2020-05-21 17:27:18 -0500

    made branch and starting to flesh everything out

---
+++ index.js
@@ -0,0 +1,3 @@
+const server = require("./server.js");
+const port = 5001;
+server.listen(port, () => console.log(`\n server up on port 5001 \n`));
---
+++ server.js
@@ -0,0 +1,2 @@
+const port = 5001;
+server.listen(port, () => console.log(`\n server up on port 5001 \n`));
